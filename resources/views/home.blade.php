76033a7443be97a970da947e08c580f6fe76d359
Hibatul Azis <aziswh7@gmail.com>
2023-03-26 23:00:06 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <title>Hitalk-2023</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
+<body >
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Template Blade</a>
@@ -38,7 +38,8 @@
 
     
 
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
+@yield('js')
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -19,7 +19,8 @@
             </div>
             <div class="mb-3">
                 <label >Fakultas <span class="text-danger">*</span></label>
-                <select class="form-control" id="exampleFormControlSelect1" name="fakultas" value="{{ old('fakultas') }}">
+                <select class="form-control" id="fakultas" name="fakultas" value="{{ old('fakultas') }}">
+                    <option>Pilih Fakultas Anda</option>
                     <option>FIK</option>
                     <option>FEB</option>
                     <option>FIB</option>
@@ -29,7 +30,10 @@
             </div>
             <div class="mb-3">
                 <label >Prodi <span class="text-danger">*</span></label>
-                <input class="form-control" type="text" name="prodi" value="{{ old('prodi') }}">
+                
+                <!-- <input class="form-control" type="text" name="prodi" value="{{ old('prodi') }}"> -->
+                <select class="form-control" id="prodi" name="prodi" value="{{ old('prodi') }}">
+                </select>
             </div>
             <div class="mb-3">
                 <label >NIM <span class="text-danger">*</span></label>
@@ -45,4 +49,51 @@
         </form>
     </div>
 </div>
+@endsection
+@section('js')
+<script>
+    $(document).ready(function() {
+        let prodi = {
+            FIK: [
+                {value: "Teknik Informatika", text: "Teknik Informatika"},
+                {value: "Sistem Informasi", text: "Sistem Informasi"},
+                {value: "Ilmu Komunikasi", text: "Ilmu Komunikasi"},
+                {value: "Desain Komunikasi Visual", text: "Desain Komunikasi Visual"},
+                {value: "Animasi", text: "Animasi"},
+                {value: "Film dan Televisi", text: "Film dan Televisi"},
+            ],
+            FEB: [
+                {value: "Manajemen", text: "Manajemen"},
+                {value: "Akuntansi", text: "Akuntansi"},
+            ],
+            FIB: [
+                {value: "Bahasa Inggris", text: "Bahasa Inggris"},
+                {value: "Sastra Jepang", text: "Sastra Jepang"},
+                {value: "Manajemen Perhotelan", text: "Manajemen Perhotelan"},
+            ],
+            FT: [
+                {value: "Teknik Elektro", text: "Teknik Elektro"},
+                {value: "Teknik Industri", text: "Teknik Industri"},
+                {value: "Teknik Biomedis", text: "Teknik Biomedis"},
+            ],
+            FKES: [
+                {value: "Kesehatan Masyarakat", text: "Kesehatan Masyarakat"},
+                {value: "Rekam Medik dan Informasi Kesehatan", text: "Rekam Medik dan Informasi Kesehatan"},
+                {value: "Kesehatan Lingkungan", text: "Kesehatan Lingkungan"},
+            ],
+        };
+
+        $('#fakultas').on('change', function() {
+            let val = $(this).val();
+            let prodis = prodi[val];
+            $('#prodi')
+            .find('option')
+            .remove();
+
+            for (const item of prodis) {
+                $('#prodi').append($('<option>', item));
+            }
+        });
+    });
+</script>
 @endsection
